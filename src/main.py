7739f5e092b846148509cbb1cbbb48348be4f06d
dandeeccastro <dundeepessoal@gmail.com>
2022-05-16 23:16:47 -0300

    :sparkles: -m tag allows creating 00 to 09 range

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 #!/bin/python3
-import os 
+import os
 import getopt
 import yaml
 import math
@@ -9,8 +9,10 @@ JD_ID = 00.00
 META = False
 
 def parseArgs():
-    try: 
-        opts, args = getopt.getopt(sys.argv[1:], "hf:p:m", 
+    global META
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hf:p:m",
                 ["help", "file=", "path="])
     except:
         sys.exit(2)
@@ -18,8 +20,8 @@ def parseArgs():
     source_file = "../res/jd.yaml"
     dest_path = "./"
 
-    for opt, arg in opts: 
-        if opt in ("-h","--help"): 
+    for opt, arg in opts:
+        if opt in ("-h","--help"):
             sys.exit(2)
         elif opt in ("-f", "--file"):
             source_file = arg
@@ -28,27 +30,30 @@ def parseArgs():
         elif opt in ("-m", "--meta"):
             META = True
         else:
-            assert False, "unknown option" 
+            assert False, "unknown option"
 
     return source_file, dest_path
 
 def generateLayerPrefix(jd_id, level):
-    if level == 1: 
-        result = "{} - {}".format(math.floor(jd_id),math.floor(jd_id + 9))
-    elif level == 2: 
+    if level == 1:
+        result = "{:02} - {:02}".format(math.floor(jd_id),math.floor(jd_id + 9))
+    elif level == 2:
         result = "{}".format(round(jd_id))
     else:
         result = "{:.2f}".format(jd_id)
     return result
 
-def generateFileSystem(jd_data, dest_path): 
+def generateFileSystem(jd_data, dest_path):
     global JD_ID
+    global META
 
     os.chdir(dest_path)
 
-    for upper in jd_data: 
+    for upper in jd_data:
 
-        JD_ID += 10 
+        if not META:
+            JD_ID += 10
+
         current_upper_macro = JD_ID
         upper_name = "{} {}".format(generateLayerPrefix(JD_ID,1), upper)
 
@@ -62,7 +67,7 @@ def generateFileSystem(jd_data, dest_path):
             middle_name = "{} {}".format(generateLayerPrefix(JD_ID,2), middle)
 
             os.mkdir(middle_name)
-            os.chdir(middle_name) 
+            os.chdir(middle_name)
 
             for end in jd_data[upper][middle]:
                 JD_ID += .01
@@ -74,12 +79,16 @@ def generateFileSystem(jd_data, dest_path):
             JD_ID = current_middle_macro
 
         os.chdir("..")
-        JD_ID = current_upper_macro 
+        JD_ID = current_upper_macro
+
+        if META:
+            JD_ID += 10
 
 if __name__ == '__main__':
 
     source_file, dest_path = parseArgs()
-    source_stream = open(source_file,'r') 
-    jd_data = yaml.load(source_stream,Loader=yaml.Loader) 
+    print(META)
+    source_stream = open(source_file,'r')
+    jd_data = yaml.load(source_stream,Loader=yaml.Loader)
 
     generateFileSystem(jd_data, dest_path)
